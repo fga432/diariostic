3ea5fff8b5e50219ebc8d9aad6cd94ca807c627a
fga432 <72905551+fga432@users.noreply.github.com>
2020-11-08 10:51:39 +0100

    Update portada.php

--- secciones/portada.php
+++ secciones/portada.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "Portada actualizada DAVID",
-"autor" => "mtorres",
+"titulo" => "Portada actualizada DAVID (fga432)",
+"autor" => "fga432",
 "resumen" => "Resumen de la portada",
 ];
 ?>
